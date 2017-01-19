b3b10e461e5430f674e4a5fdfa77d46a394091ef
Colin Davies <colin.davies-2@manchester.ac.uk>
2017-01-19 16:54:12 +0000

    STROK-54 [CLIENT and SERVER] Implement BP treatment chart (per patient)
    Change-Id: I0a89b28f14b289be5d94568997e50ef3853ce1b9

--- stroke-app-jhipster/src/main/webapp/app/entities/patient/patient-detail.controller.js
+++ stroke-app-jhipster/src/main/webapp/app/entities/patient/patient-detail.controller.js
@@ -5,18 +5,96 @@
         .module('strokeApp')
         .controller('PatientDetailController', PatientDetailController);
 
-    PatientDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'previousState', 'entity', 'Patient', 'bpManagementEntries', 'Hospital', 'Inr'];
+    PatientDetailController.$inject = ['$scope', '$rootScope', '$stateParams', 'previousState', 'entity', 'Patient', 'bpManagementEntries', 'Hospital', 'Inr', 'DateUtils'];
 
-    function PatientDetailController($scope, $rootScope, $stateParams, previousState, entity, Patient, bpManagementEntries, Hospital, Inr) {
+    function PatientDetailController($scope, $rootScope, $stateParams, previousState, entity, Patient, bpManagementEntries, Hospital, Inr, DateUtils) {
         var vm = this;
 
         vm.patient = entity;
         vm.bpManagementEntries = bpManagementEntries;
+        vm.chart = null;
         vm.previousState = previousState.name;
+        vm.bpTabClicked = bpTabClicked;
 
         var unsubscribe = $rootScope.$on('strokeApp:patientUpdate', function(event, result) {
             vm.patient = result;
         });
         $scope.$on('$destroy', unsubscribe);
+        
+        vm.chartDataPoints = getChartDataPoints(bpManagementEntries);
+               
+        function generateChart(chartDataPoints) {
+            vm.chart = c3.generate({
+                bindto: "#bp-management-chart",
+                size: {
+                    height: 400
+                },
+                data: {
+                	xFormat: '%Y-%m-%d %H:%M:%S', // how the date is parsed
+                	json: chartDataPoints,
+                	keys: { x: "x", value: ["bp", "gtn", "labetalol"] },
+                	names: { bp: "Systolic BP", gtn: "GTN Rate", labetalol: "Labetalol Dose" },
+                	types: { bp: "line", gtn: "bar", labetalol: "bar"},
+                	axes: { bp: 'y', gtn: 'y2', labetalol: 'y2' }
+                },
+                axis: {
+                    x: {
+                        type: 'timeseries',
+                        tick: {
+                        	culling: false,
+                            format: '%H:%M', // how the date is displayed
+                            rotate: 90,
+                            multiline: false
+                        },
+                        height: 70
+                    },
+                    y: {
+                        label: {
+                            text: "mmHg",
+                            position: 'outer-middle'
+                        }
+                    },
+                    y2: { show: true}
+                },
+                grid: {
+                    x: {
+                        show: true
+                    },
+                    y: {
+                        show: true,
+                    }
+                }
+            });
+        }
+
+        function getChartDataPoints(bpManagementEntries) {
+        	
+        	var i, bpManagementEntry, chartDataPoints = [];
+        	
+        	for(i = 0; i < bpManagementEntries.length; i++) {
+        		bpManagementEntry = bpManagementEntries[i];
+        		chartDataPoints.push({
+        			x: getTimeSeriesValue(bpManagementEntry), 
+        			bp: bpManagementEntry.systolicBp,
+        			gtn: bpManagementEntry.gtnRate,
+        			labetalol: bpManagementEntry.labetalolDose
+        		});
+        	}
+        	
+        	return chartDataPoints;
+        }
+        
+        function getTimeSeriesValue(bpManagementEntry) {
+        	
+        	var bpManagementEntryDate = DateUtils.convertDateTimeFromServer(bpManagementEntry.dateTime);
+        	return bpManagementEntryDate;
+        }
+        
+        function bpTabClicked() {
+        	// The chart does not render if the tab is hidden
+        	// so we tie the chart generation to the selection of the tab.
+            generateChart(vm.chartDataPoints);
+        }
+
     }
 })();

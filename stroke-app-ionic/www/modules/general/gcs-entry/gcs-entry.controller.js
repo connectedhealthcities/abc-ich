7e6d8b07225b575c706226492ef47b506d0be2e6
EddTempest <edward.tempest@manchester.ac.uk>
2017-02-07 16:32:37 +0000

    STROK-67  Changes to GCS Entry screen S3
    Change-Id: Iafbdec837d795912211a892f5b1a88d0601d0d4b

--- stroke-app-ionic/www/modules/general/gcs-entry/gcs-entry.controller.js
+++ stroke-app-ionic/www/modules/general/gcs-entry/gcs-entry.controller.js
@@ -9,8 +9,20 @@ function GcsEntryController($state) {
     var vm = this; // S3
 
     vm.onNext = onNext;
+    vm.gcsValueChanged = gcsValueChanged;
+    
+    vm.eye = null;
+    vm.verbal = null;
+    vm.motor = null;
+    vm.total = null;
 
     function onNext() {
          $state.go('tabs.anticoagulant-identification');
+    }
+
+    function gcsValueChanged() {
+    	if(vm.eye && vm.verbal && vm.motor){
+    		vm.total = vm.eye + vm.verbal + vm.motor;
+    	}
     }
 }

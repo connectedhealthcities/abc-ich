e92fca6a01d05cd72384cab534a92367636d1d8d
Colin Davies <colin.davies-2@manchester.ac.uk>
2017-02-07 21:21:12 +0000

    STROK-102 [TABLET APP] Implement S3 screen - Enter GCS
    Change-Id: I070d9a0ecf836b68eb1dc6c01905c92121ea900f

--- stroke-app-ionic/tests/controllers/gcs-entry-controller.spec.js
+++ stroke-app-ionic/tests/controllers/gcs-entry-controller.spec.js
@@ -3,25 +3,143 @@
 describe('GcsEntryController', function() {
 
     var vm;
-    var state;
+    var scope, ionicPopup, patientCacheService, state, q;
 
     beforeEach(function() {
 
         module('app.general');
 		module('ui.router');
 
-		angular.mock.inject(function($controller, _$state_) {
+		angular.mock.inject(function($controller, $rootScope, _$state_, _$q_) {
 
 			state = _$state_;
-			vm = $controller('GcsEntryController', {});				
+			scope = $rootScope.$new();
+			q = _$q_;
+			ionicPopup = {
+				confirm: function() {},
+				alert: function() {}
+			};
+			patientCacheService = {
+				setGcsScoreEye: function() {},
+				setGcsScoreVerbal: function() {},
+				setGcsScoreMotor: function() {},
+				setGcsScore: function() {}
+			};			
+
+			vm = $controller('GcsEntryController', {'$scope': scope, '$ionicPopup': ionicPopup, 'PatientCacheService': patientCacheService});	 	
 		});
      });
 
-	it("should go to state 'anticoagulant-identification' on 'Next' button click", function() {
-			
+	it("should not change view when data is not confirmed", function() {
+						
+		spyOn(patientCacheService, 'setGcsScoreEye');
+		spyOn(patientCacheService, 'setGcsScoreVerbal');
+		spyOn(patientCacheService, 'setGcsScoreMotor');
+		spyOn(patientCacheService, 'setGcsScore');
+
+		spyOn(ionicPopup, 'confirm').and.callFake(function() {
+			var deferred = q.defer();
+			deferred.resolve(false); // Cancel button selected
+			return deferred.promise;
+		});
+		spyOn(ionicPopup, 'alert');
+
 		spyOn(state, 'go');
 
 		vm.onNext(); // call the click handler
+
+		expect(ionicPopup.confirm).toHaveBeenCalled();
+
+		scope.$apply(); // Propagate 'ionicPopup.confirm' promise			
+
+	    expect(ionicPopup.alert).not.toHaveBeenCalled();		
+
+	    expect(patientCacheService.setGcsScoreEye).not.toHaveBeenCalled();		
+	    expect(patientCacheService.setGcsScoreVerbal).not.toHaveBeenCalled();		
+	    expect(patientCacheService.setGcsScoreMotor).not.toHaveBeenCalled();		
+	    expect(patientCacheService.setGcsScore).not.toHaveBeenCalled();		
+
+	    expect(state.go).not.toHaveBeenCalled();		
+    });
+
+	it("should go to state 'anticoagulant-identification' when data is confirmed #1", function() {
+			
+		spyOn(patientCacheService, 'setGcsScoreEye');
+		spyOn(patientCacheService, 'setGcsScoreVerbal');
+		spyOn(patientCacheService, 'setGcsScoreMotor');
+		spyOn(patientCacheService, 'setGcsScore');
+			
+		spyOn(ionicPopup, 'confirm').and.callFake(function() {
+			var deferred = q.defer();
+			deferred.resolve(true); // Ok button selected
+			return deferred.promise;
+		});
+		spyOn(ionicPopup, 'alert');
+
+		spyOn(state, 'go');
+
+		// GCS >=9
+		vm.eye = 3;
+    	vm.verbal = 4;
+   	 	vm.motor = 5;
+    	vm.total = 12;
+
+		vm.onNext(); // call the click handler
+
+		expect(ionicPopup.confirm).toHaveBeenCalled();
+
+		scope.$apply(); // Propagate 'ionicPopup.confirm' promise			
+
+	    expect(ionicPopup.alert).not.toHaveBeenCalled();
+
+ 	    expect(patientCacheService.setGcsScoreEye).toHaveBeenCalledWith(3);		
+	    expect(patientCacheService.setGcsScoreVerbal).toHaveBeenCalledWith(4);		
+	    expect(patientCacheService.setGcsScoreMotor).toHaveBeenCalledWith(5);		
+	    expect(patientCacheService.setGcsScore).toHaveBeenCalledWith(12);		
+
+	    expect(state.go).toHaveBeenCalledWith('tabs.anticoagulant-identification');		
+    });
+
+	it("should go to state 'anticoagulant-identification' when data is confirmed #2", function() {
+			
+		spyOn(patientCacheService, 'setGcsScoreEye');
+		spyOn(patientCacheService, 'setGcsScoreVerbal');
+		spyOn(patientCacheService, 'setGcsScoreMotor');
+		spyOn(patientCacheService, 'setGcsScore');
+			
+		spyOn(ionicPopup, 'confirm').and.callFake(function() {
+			var deferred = q.defer();
+			deferred.resolve(true); // Ok button selected
+			return deferred.promise;
+		});
+		spyOn(ionicPopup, 'alert').and.callFake(function() {
+			var deferred = q.defer();
+			deferred.resolve();
+			return deferred.promise;
+		});
+
+		spyOn(state, 'go');
+
+		// GCS < 9
+		vm.eye = 1;
+    	vm.verbal = 1;
+   	 	vm.motor = 1;
+    	vm.total = 3;
+
+		vm.onNext(); // call the click handler
+
+		expect(ionicPopup.confirm).toHaveBeenCalled();
+
+		scope.$apply(); // Propagate 'ionicPopup.confirm' promise			
+
+		expect(ionicPopup.alert).toHaveBeenCalled();
+
+	    expect(patientCacheService.setGcsScoreEye).toHaveBeenCalledWith(1);		
+	    expect(patientCacheService.setGcsScoreVerbal).toHaveBeenCalledWith(1);		
+	    expect(patientCacheService.setGcsScoreMotor).toHaveBeenCalledWith(1);		
+	    expect(patientCacheService.setGcsScore).toHaveBeenCalledWith(3);		
+
+		scope.$apply(); // Propagate 'ionicPopup.alert' promise			
 
 	    expect(state.go).toHaveBeenCalledWith('tabs.anticoagulant-identification');		
     });
--- stroke-app-ionic/www/modules/general/gcs-entry/gcs-entry.controller.js
+++ stroke-app-ionic/www/modules/general/gcs-entry/gcs-entry.controller.js
@@ -2,27 +2,79 @@
 
 angular.module('app.general').controller('GcsEntryController', GcsEntryController);
 
-GcsEntryController.$inject = ['$state'];
+GcsEntryController.$inject = ['$scope', '$state', '$ionicPopup', 'PatientCacheService'];
 
-function GcsEntryController($state) {
+function GcsEntryController($scope, $state, $ionicPopup, PatientCacheService) {
 
     var vm = this; // S3
 
-    vm.onNext = onNext;
-    vm.gcsValueChanged = gcsValueChanged;
-    
     vm.eye = null;
     vm.verbal = null;
     vm.motor = null;
     vm.total = null;
 
-    function onNext() {
-         $state.go('tabs.anticoagulant-identification');
-    }
+    vm.gcsValueChanged = gcsValueChanged;
+    vm.isNextButtonEnabled = isNextButtonEnabled;
+    vm.onNext = onNext;
 
     function gcsValueChanged() {
     	if(vm.eye && vm.verbal && vm.motor){
     		vm.total = vm.eye + vm.verbal + vm.motor;
     	}
+    }
+
+    function isNextButtonEnabled() {
+        return vm.total != null;
+    }
+    
+    function onNext() {
+        showDataValidationPopup(dataValid);
+    }
+
+    function dataValid() {
+        saveData();
+        if (vm.total < 9) {
+            showStabilisePatientPopup(goNextState);
+        }
+        else {
+            goNextState();
+        }
+     }
+
+     function goNextState() {
+        $state.go('tabs.anticoagulant-identification');
+     }
+
+    function saveData() {
+        PatientCacheService.setGcsScoreEye(vm.eye);       
+        PatientCacheService.setGcsScoreVerbal(vm.verbal);
+        PatientCacheService.setGcsScoreMotor(vm.motor);
+        PatientCacheService.setGcsScore(vm.total);
+    }
+
+    function showDataValidationPopup(okHandler) {
+        var popupTemplate = {
+            templateUrl: 'modules/general/gcs-entry/data-validation-popup.html',
+            title: 'Data validation',
+            subTitle: 'Please confirm data entered is correct',
+            scope: $scope
+        };       
+        var popup = $ionicPopup.confirm(popupTemplate);
+
+        popup.then(function(res) {
+            if (res) {
+                okHandler();
+            }
+        });
+    }
+
+    function showStabilisePatientPopup(okHandler) {
+        var popupTemplate = {
+            templateUrl: 'modules/general/gcs-entry/stabilise-patient-popup.html',
+            title: 'Stabilise patient'
+        };
+        var popup = $ionicPopup.alert(popupTemplate);
+
+        popup.then(okHandler);
     }
 }

fa11e557e867a9925d6694c94574d2d69896e655
EddTempest <edward.tempest@manchester.ac.uk>
2017-02-13 16:47:30 +0000

    STROK-96 Implementing Refer to critical care screen
    Change-Id: Ic82518f5277a822b87bd8f47479e41d5d35e18b7

--- stroke-app-ionic/www/modules/protocol-b/critical-care-referral/critical-care-referral.controller.js
+++ stroke-app-ionic/www/modules/protocol-b/critical-care-referral/critical-care-referral.controller.js
@@ -1,26 +1,83 @@
+/// <reference path="critical-care-referral-data-validation-popup.html" />
 'use strict';
 
 angular.module('app.protocolB') .controller('CriticalCareReferralController', CriticalCareReferralController);
 
-CriticalCareReferralController.$inject = ['$state', 'PatientCacheService', 'TabStateCacheService'];
+CriticalCareReferralController.$inject = ['$scope', '$state', '$ionicPopup', 'PatientCacheService', 'TabStateCacheService', 'GCS_THRESHOLD'];
 
-function CriticalCareReferralController($state, PatientCacheService, TabStateCacheService) {
+function CriticalCareReferralController($scope, $state, $ionicPopup, PatientCacheService, TabStateCacheService, GCS_THRESHOLD) {
  
     var vm = this; // S4
 
     TabStateCacheService.setStateTabB('tabs.critical-care-referral');
 
+    vm.destination;
+    vm.destinationOther;
+
     vm.onNext = onNext;
+    vm.isNextButtonEnabled = isNextButtonEnabled;
+    vm.OnOtherChanged = OnOtherChanged;
+
+    function OnOtherChanged() {
+        vm.destinationOther = null;
+    }
+
+    function isNextButtonEnabled() {
+        var isEnabled = false;
+        if (vm.destination) {
+            if (vm.destination !== "Other") {
+                isEnabled = true;
+            }
+            else {
+                if (vm.destinationOther) {
+                    isEnabled = true;
+                }
+            }
+        }
+        return isEnabled;
+    }
 
     function onNext() {
+        showDataValidationPopup(dataValid);
+    }
 
-        if (PatientCacheService.getGcsScore() < 9) {
+    function goNextState(){
+
+        if (PatientCacheService.getGcsScore() < GCS_THRESHOLD) {
             $state.go('patient-end');
         }
         else {
-            var state = TabStateCacheService.getStateTabC();
-            $state.go(state);
+            TabStateCacheService.goLatestStateTabC();
         }
 
+    }
+
+    function dataValid() {
+        saveData();
+        goNextState();
+    }
+
+    function saveData() {
+        PatientCacheService.setDestination(vm.destination);
+        if (vm.destination === "Other") {
+            PatientCacheService.setOtherDestination(vm.destinationOther);
+        }
+    }
+
+    function showDataValidationPopup(okHandler) {
+        var popupTemplate = {
+            templateUrl: 'modules/protocol-b/critical-care-referral/critical-care-referral-data-validation-popup.html',
+            title: 'Data validation',
+            subTitle: 'Please confirm data entered is correct',
+            scope: $scope,
+            cssClass: 'chi-wide-popup'
+        };
+        var popup = $ionicPopup.confirm(popupTemplate);
+
+        popup.then(function (res) {
+            if (res) {
+                okHandler();
+            }
+        });
     }
 }

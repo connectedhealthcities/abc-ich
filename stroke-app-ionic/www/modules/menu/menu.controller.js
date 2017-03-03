4ba93e33383a01f9b789774cc6667dd7396bd60e
Colin Davies <colin.davies-2@manchester.ac.uk>
2017-03-03 18:43:18 +0000

    Clear all demo-mode caches whenever Demo-mode is selected
    Change-Id: Idf7f3c5c3232c9c3f4c56e2bf9ad41c830be1a09

--- stroke-app-ionic/www/modules/menu/menu.controller.js
+++ stroke-app-ionic/www/modules/menu/menu.controller.js
@@ -2,16 +2,19 @@
 
 angular.module('app').controller('MenuController', MenuController);
 
-MenuController.$inject = ['$scope', '$state', 'PatientCacheService'];
+MenuController.$inject = ['$scope', '$state', 'PatientCacheService', 'BpStateCacheService', 'TabStateCacheService'];
 
-function MenuController($scope, $state, PatientCacheService) {
-
- //   var vm = this;
+function MenuController($scope, $state, PatientCacheService, BpStateCacheService, TabStateCacheService) {
 
     $scope.onDemoMode = onDemoMode;
 
     function onDemoMode() {
         PatientCacheService.setIsDemoMode(true);
+        
+        PatientCacheService.clearAll();
+        BpStateCacheService.clearAll();
+        TabStateCacheService.clearAll();
+
         $state.go("register-patient");
     }
 }

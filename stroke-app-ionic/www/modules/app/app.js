16d21fd935efa1767d366575761db85330075000
Dan Cave <daniel.cave@manchester.ac.uk>
2018-02-08 17:21:28 +0000

    EM-153 [ABC-ICH] Change response to disclaimer from "OK" to "I agree"
    Change-Id: Ib0fe6dd9e3c17adb2881caf1a614ae578b48dad8

--- stroke-app-ionic/www/modules/app/app.js
+++ stroke-app-ionic/www/modules/app/app.js
@@ -6,36 +6,36 @@ httpInterceptor.$inject = ['$injector'];
 
 function httpInterceptor($injector) {
   return {
-    'request': function(config) {
-        config.timeout = 15000;
-        if( (config.url.indexOf('/api/') > -1) && 
-            (config.url.indexOf('/api/authenticate') === -1) ) {
-          // use $injector to prevent circular dependency
-          var authenticationService = $injector.get('AuthenticationService');
-          return authenticationService.authenticate().then(function() {
-            config.headers.Authorization = 'Bearer ' + authenticationService.getJwt();
-            return config;
-          });
-        }
-        else {
+    'request': function (config) {
+      config.timeout = 15000;
+      if ((config.url.indexOf('/api/') > -1) &&
+        (config.url.indexOf('/api/authenticate') === -1)) {
+        // use $injector to prevent circular dependency
+        var authenticationService = $injector.get('AuthenticationService');
+        return authenticationService.authenticate().then(function () {
+          config.headers.Authorization = 'Bearer ' + authenticationService.getJwt();
           return config;
-        }
+        });
       }
+      else {
+        return config;
+      }
+    }
   };
 }
 
 angular.module('app').config(AppConfig);
 
-AppConfig.$inject = ['$urlRouterProvider', '$ionicConfigProvider', '$sceDelegateProvider','$httpProvider'];
+AppConfig.$inject = ['$urlRouterProvider', '$ionicConfigProvider', '$sceDelegateProvider', '$httpProvider'];
 
 function AppConfig($urlRouterProvider, $ionicConfigProvider, $sceDelegateProvider, $httpProvider) {
 
   $urlRouterProvider.otherwise("/patient-start");
 
 //cjd - do we need this??
-  $sceDelegateProvider.resourceUrlWhitelist([ 'self','*://www.youtube.com/**', '*://player.vimeo.com/video/**']);
+  $sceDelegateProvider.resourceUrlWhitelist(['self', '*://www.youtube.com/**', '*://player.vimeo.com/video/**']);
 
-  $httpProvider.interceptors.push('httpInterceptor'); 
+  $httpProvider.interceptors.push('httpInterceptor');
 
 }
 
@@ -46,7 +46,7 @@ AppRun.$inject = ['$ionicPlatform', '$ionicPopup'];
 
 function AppRun($ionicPlatform, $ionicPopup) {
 
-  $ionicPlatform.ready(function() {
+  $ionicPlatform.ready(function () {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
@@ -58,31 +58,38 @@ function AppRun($ionicPlatform, $ionicPopup) {
       StatusBar.styleDefault();
     }
 
-    $ionicPlatform.registerBackButtonAction(function() {
-      showExitPopup(ionic.Platform.exitApp)        
+    $ionicPlatform.registerBackButtonAction(function () {
+      showExitPopup(ionic.Platform.exitApp)
     }, 101);
 
     function showExitPopup(okHandler) {
       var popupTemplate = {
-         title: 'Exit ABC-ICH?'
+        title: 'Exit ABC-ICH?'
       };
 
       var popup = $ionicPopup.confirm(popupTemplate);
 
-      popup.then(function(res) {
-          if (res) {
-              okHandler();
-          }
+      popup.then(function (res) {
+        if (res) {
+          okHandler();
+        }
       });
     }
 
     function showDisclaimerPopup() {
-        var popupTemplate = {
-            templateUrl: 'modules/app/disclaimer-popup.html',
-            title: 'Disclaimer',
-            cssClass: 'chi-wide-popup'
-        };
-        $ionicPopup.alert(popupTemplate);
+      var popupTemplate = {
+        templateUrl: 'modules/app/disclaimer-popup.html',
+        title: 'Disclaimer',
+        cssClass: 'chi-wide-popup',
+        buttons: [
+          {
+            text: 'I Agree',
+            type: 'button-calm'
+          }
+        ]
+      };
+
+      $ionicPopup.alert(popupTemplate);
     }
 
     showDisclaimerPopup();
